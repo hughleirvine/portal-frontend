637ad3a8dd28c2d3168b34e8d4fb59ce7e9da43b
hughleirvine <hughleirvine@gmail.com>
2025-09-02 11:26:33 -0700

    Initial commit of clean portal

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, a{ useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Login from './Login';
